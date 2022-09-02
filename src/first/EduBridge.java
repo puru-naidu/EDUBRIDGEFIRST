4e09c7e62cc5055ba9ebdc705de0083dc9c4666a
puros <puros@LAPTOP-EL3RGT5F>
2022-09-02 11:16:34 +0530

    First

---
+++ src/first/EduBridge.java
@@ -0,0 +1,8 @@
+package first;
+
+public class EduBridge {
+	public static void main(String[]args) {
+		System.out.println("Edubridge is an institution where it provides training and placement for its students");
+   	}
+	
+}
