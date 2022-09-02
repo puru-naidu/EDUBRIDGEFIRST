4bd0814637e49e4715981cf0d7a169f5de8b8854
puros <puros@LAPTOP-EL3RGT5F>
2022-09-02 12:19:32 +0530

    second commit

---
+++ src/second/Primitive.java
@@ -0,0 +1,25 @@
+package second;
+
+public class Primitive {
+	public static void main(String[]args) {
+		byte b=127;
+		System.out.println(b);
+		short s = 3245;
+		System.out.println(s);
+		int i = 12345;
+		System.out.println(i);
+		long l = 1245455555L;
+		System.out.println(l);
+		float c=12.25f;
+		System.out.println(c);
+		double d = 12.2154545;
+		System.out.println(d);
+		char a = '$';
+		System.out.println(a);
+		boolean m = true;
+		System.out.println(m);
+		
+		
+	}
+
+}
